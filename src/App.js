947707cd69a35997cfb94ff684a0b89ba886ffb0
Chananya Nissaisue <chananya_n@hotmail.com>
2022-04-11 20:05:01 +0700

    1

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        hiii jib
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
-          1234
+          Learn React
         </a>
       </header>
     </div>
